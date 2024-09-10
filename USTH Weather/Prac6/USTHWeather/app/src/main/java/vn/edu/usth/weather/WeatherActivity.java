05014b113fad45b92b3c2adb899db30f45b270e0
NguyenVuThe <nguyenvtk2110@gmail.com>
2024-09-11 01:22:56 +0700

    Update Practical 6

---
+++ USTH Weather/Prac6/USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -0,0 +1,58 @@
+package vn.edu.usth.weather;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class WeatherActivity extends AppCompatActivity {
+    private static final String TAG = "Weather";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_weather);
+        Log.i(TAG, "Create");
+
+//        ForecastFragment forecastFragment = new ForecastFragment();
+//        WeatherFragment weatherFragment = new WeatherFragment();
+//
+//        getSupportFragmentManager().beginTransaction().add(R.id.main, forecastFragment).commit();
+//        getSupportFragmentManager().beginTransaction().add(R.id.main, weatherFragment).commit();
+    }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        Log.i(TAG, "Start");
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        Log.i(TAG, "Resume");
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        Log.i(TAG, "Pause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "Stop");
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        Log.i(TAG, "Destroy");
+    }
+}
